4211fc9df86fa7c298978d9fb959df4d3b4ec50c
veroniqq <veronika180207@gmail.com>
2026-02-17 13:13:57 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество двоек: ");
+        long a = long.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество троек: ");
+        long b = long.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество четверок: ");
+        long c = long.Parse(Console.ReadLine());  
+
+        long kvo = a + b + c;
+
+        long sm = a * 2 + b * 3 + c * 4;
+
+        long k = 0;
+
+        while (sm*2 < 7*kvo)
+        {
+            k++;
+            sm += 5;
+            kvo ++;
+        }
+
+        Console.WriteLine(k);
+
+
+
+
+
+    }
+}
